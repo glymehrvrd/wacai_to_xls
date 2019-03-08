9b7c1ed93a9f9a63f38d2c05ed1eb181b1e48fe3
jasonjsyuan <jasonjsyuan@tencent.com>
2019-03-08 21:18:34 +0800

    up

---
+++ README.md
@@ -0,0 +1,9 @@
+# Introduce
+从挖财安卓APP导出账本到xls
+
+# APP DB
+挖财安卓APP数据库位于`/data/data/com.wacai365/databases/wacai365.so`，获取需要root权限
+
+# xls
+使用挖财官方的xls模板，见`wacai.xls`，生成的xls可以在挖财官网网页上导入
+
---
+++ wacai.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 19:32:52 2019
+
+@author: jasonjsyuan
+"""
+
+import pandas as pd
+import sqlite3
+from datetime import datetime
+
+conn=sqlite3.connect('wacai365.so')
+
+df=pd.read_sql_query('select uuid,name from TBL_ACCOUNTINFO',conn)
+accounts={}
+for _, row in df.iterrows():
+    accounts[row['uuid']]=row['name']
+    
+df=pd.read_sql_query('select uuid,name from TBL_OUTGOMAINTYPEINFO',conn)
+outgomaintype={}
+for _,row in df.iterrows():
+    outgomaintype[row['uuid']]=row['name']
+    
+df=pd.read_sql_query('select uuid,name,parentUuid from TBL_OUTGOSUBTYPEINFO',conn)
+outgosubtype={}
+outgosubtomain={}
+for _,row in df.iterrows():
+    outgosubtype[row['uuid']]=row['name']
+    outgosubtomain[row['uuid']]=row['parentUuid']
+    
+df=pd.read_sql_query('select uuid,name from TBL_INCOMEMAINTYPEINFO',conn)
+incomemaintype={}
+for _,row in df.iterrows():
+    incomemaintype[row['uuid']]=row['name']
+    
+df=pd.read_sql_query('select uuid,name from TBL_BOOK',conn)
+books={}
+for _,row in df.iterrows():
+    books[row['uuid']]=row['name']
+    
+df=pd.read_sql_query('select * from TBL_TRADEINFO',conn)
+
+dd_1=[]
+dd_2=[]
+dd_3=[]
+
+for _,row in df.iterrows():
+    if row['isdelete']==1:
+        continue
+    
+    book=books[row['bookUuid']]
+    account=accounts[row['accountUuid']]
+    dd=datetime.fromtimestamp(row['date']).strftime('%Y-%m-%d %H:%M:%S')
+    #print(book,account)
+
+    tradetype=row['tradetype']
+    if tradetype==1:
+        # outcome
+        maintyp=outgomaintype[outgosubtomain[row['typeUuid']]]
+        subtyp=outgosubtype[row['typeUuid']]
+        dd_1.append((maintyp, subtyp, '现金', '人民币', '日常', '', 
+              '非报销', dd, '%.2f'%(float(row['money'])/100),
+              '', row['comment'] or '', book))
+    elif tradetype==2:
+        # income
+        typ=incomemaintype[row['typeUuid']]
+        dd_2.append((typ, '现金', '人民币', '日常', '', 
+              dd, '%.2f'%(float(row['money'])/100),
+              '', row['comment'] or '', book))
+    elif tradetype==3:
+        account2=accounts[row['accountUuid2']]
+        dd_3.append((account, '人民币', '%.2f'%(float(row['money'])/100),
+               account2, '人民币', '%.2f'%(float(row['money2'])/100), dd, row['comment'] or '', book))
+    else:
+        pass
+
+
+df_1=pd.DataFrame(dd_1,columns=['支出大类','支出小类','账户','币种','项目','商家','报销','消费日期','消费金额','成员金额','备注','账本'])
+df_2=pd.DataFrame(dd_2,columns=['收入大类','账户','币种','项目','付款方','收入日期','收入金额','成员金额','备注','账本'])
+df_3=pd.DataFrame(dd_3,columns=['转出账户','币种','转出金额','转入账户','币种','转入金额','转账时间','备注','账本'])
+
+writer = pd.ExcelWriter('out.xls')
+df_1.to_excel(writer,sheet_name='支出',index=False)
+df_2.to_excel(writer,sheet_name='收入',index=False)
+df_3.to_excel(writer,sheet_name='转账',index=False)
+writer.save()
